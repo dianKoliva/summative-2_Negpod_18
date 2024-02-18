9b75029f4c38452fbd8caa6a0a2f9e942e778021
dianKoliva <dianpretty01@gmail.com>
2024-02-18 21:44:02 +0200

    added application menu

--- negpod_18-q1/main.sh
+++ negpod_18-q1/main.sh
@@ -1,2 +1,12 @@
 #registration application for student
 students_file="students-list_1023.txt"
+while true; do
+    echo "--------------------------------------------------------"
+    echo "Welcome to the ALU Student Registration System"
+    echo "--------------------------------------------------------"
+    echo "1. Create student record"
+    echo "2. View all students"
+    echo "3. Delete student record"
+    echo "4. Update student record"
+    echo "5. Exit"
+    read -p "Please enter your choice (1/2/3/4/5): " choice

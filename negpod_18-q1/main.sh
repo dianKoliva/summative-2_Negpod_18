c181c811645e07f2c0e81f7262c153b7aec92cc3
dianKoliva <dianpretty01@gmail.com>
2024-02-18 21:53:38 +0200

    listing all students

--- negpod_18-q1/main.sh
+++ negpod_18-q1/main.sh
@@ -10,3 +10,32 @@ while true; do
     echo "4. Update student record"
     echo "5. Exit"
     read -p "Please enter your choice (1/2/3/4/5): " choice
+
+case $choice in
+        1)
+            read -p "Enter student email: " email
+	    #checking if the email inserted is valid
+	    if [[ $email =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+            read -p "Enter student age: " age
+            read -p "Enter student ID: " student_id
+	    #checking if ID is unique
+            if grep -q ", $student_id$" "$students_file"; then
+                echo "Student with ID $student_id already exists."
+            else
+                # If the ID is not found, add the student record
+                echo "$email, $age, $student_id" >> "$students_file"
+                echo "Student information saved."
+	    fi
+	    else
+                echo "Please enter a valid email address."
+		fi
+            ;;
+        2)
+		  #checking if the file exists and is not empty
+            if [ -s "$students_file" ]; then
+                cat "$students_file"
+            else
+                echo "No students found."
+            fi
+            ;;
+        

3eb8890d636d9a3f4690f1488a264bb42e21aa4e
elyse003 <e.uyiringiy@alustudent.com>
2024-02-18 23:14:14 +0200

    update 4

--- negpod_18-q1/main.sh
+++ negpod_18-q1/main.sh
@@ -1,5 +1,6 @@
 #registration application for student
 students_file="students-list_1023.txt"
+
 while true; do
     echo "--------------------------------------------------------"
     echo "Welcome to the ALU Student Registration System"
@@ -10,7 +11,6 @@ while true; do
     echo "4. Update student record"
     echo "5. Exit"
     read -p "Please enter your choice (1/2/3/4/5): " choice
-
 case $choice in
         1)
             read -p "Enter student email: " email
@@ -20,17 +20,17 @@ case $choice in
             read -p "Enter student ID: " student_id
 	    #checking if ID is unique
             if grep -q ", $student_id$" "$students_file"; then
-                echo "Student with ID $student_id already exists."
+                echo "Student with ID $student_id already exists. Please enter a unique ID."
             else
                 # If the ID is not found, add the student record
                 echo "$email, $age, $student_id" >> "$students_file"
-                echo "Student information saved."
+                echo "Student information saved"
 	    fi
 	    else
-                echo "Please enter a valid email address."
+                echo "Please enter a valid email."
 		fi
             ;;
-        2)
+	  2)
 		  #checking if the file exists and is not empty
             if [ -s "$students_file" ]; then
                 cat "$students_file"
@@ -38,7 +38,7 @@ case $choice in
                 echo "No students found."
             fi
             ;;
-        3)
+	     3)
 	       read -p "Enter student ID to delete: " delete_id
 	       #checking if ID exists
 	       if grep -q ", $delete_id$" "$students_file"; then
@@ -48,22 +48,22 @@ case $choice in
 	    echo "ID not found"
 	       fi
             ;;
-        4)
+	     4)
 	    read -p "Enter ID to update: " update_id
 	     if grep -q ", $update_id$" "$students_file"; then
 		    read -p "Choose what to update (1 for email, 2 for age): " update_choice
-		    case $update_choice in    
-        1)
+		    case $update_choice in
+            1)
                 read -p "Enter updated email: " new_email
  if [[ $new_email =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
-sed -i "s/^[^,]*, [^,]*, $update_id$/$new_email, &/" "$students_file"
+sed -i "s/^[^,], [^,], $update_id$/$new_email, &/" "$students_file"
 echo "Student email updated"
 else
-echo "Invalid email format. Try Again"
+echo "Invalid email format"
 fi
-		;;   
-     2)
-                read -p "Enter updated age: " new_age
+		;;
+	2)
+                read -p "Enter new age: " new_age
 sed -i "s/, $update_id$/, $new_age, $update_id/" "$students_file"
                     echo "Age updated."
 		;;
@@ -74,4 +74,11 @@ esac
            else
                 echo "Student with ID $update_id not found."
             fi
-            ;;    
+            ;;
+    5)
+	    echo "Exiting ......"
+	    exit ;;
+    *)
+	    echo "Invalid Choice"
+esac
+done

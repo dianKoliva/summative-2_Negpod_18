989f6085e908fb1271789915fa9236de4167eeae
oreste-abizera <oresteabizera11@gmail.com>
2024-02-18 22:22:45 +0200

    Move files to directory

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -5,3 +5,7 @@ directory="negpod_18-q1"
 if [ ! -d "$directory" ]; then
 	mkdir "$directory"
 fi
+
+mv main.sh students-list_1023.txt select-emails.sh student-emails.txt "$directory"
+
+echo "files moved successfully"

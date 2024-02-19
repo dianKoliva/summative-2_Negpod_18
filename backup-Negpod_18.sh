c7573f00e293af41e972100467944e66bc463f54
MaxKrypton <q.rurangirw@alustudent.com>
2024-02-19 10:41:26 +0200

    Quentin 2 commit

---
+++ backup-Negpod_18.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+##Define the remote server
+#remote_server="2f05c1f8800b@2f05c1f8800b.3be8ebfc.alu-cod.online"
+#remote_dir="/summative/0923-2023S/"
+##Source Directory
+#source_dir="negpod_1023-q1"
+##Backup Directory
+#backup_dir="backup-negpod_1023-q1"
+##create_dir
+#mkdir "$backup_dir"
+##copy content of source_dir
+#cp -r "$source_dir" "$backup_dir"
+##Transfer to the remote server
+#scp -r "$backup_dir" "$remote_server:$remote_dir"
+##Remove the backup from our sandbox
+#rm -r "$backup_dir"
+#
+#echo "The directory has been backed up"
